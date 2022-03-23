725efac34efa80f20e3b5339628d629d7691c4e3
Muna Rai <raimuna669@gmailcom>
2022-03-23 19:50:59 +0545

    trying to implement borrow request

--- src/app/librarian/book-search/book-search.component.ts
+++ src/app/librarian/book-search/book-search.component.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { Librarian } from '../model/librarian.interface';
 import { ApiService } from 'src/app/shared/api.service';
-import {NgForm} from '@angular/forms';
+import { NgForm } from '@angular/forms';
+import { StudentInterface } from '../student-page/student-interface';
+
 @Component({
   selector: 'app-book-search',
   templateUrl: './book-search.component.html',
@@ -9,37 +11,62 @@ import {NgForm} from '@angular/forms';
 })
 export class BookSearchComponent implements OnInit {
   librarianData !: any;
-  
-  BookSearchText:string='';
-  bookPresent:boolean=false;
-  listOfParticularBook:Array<{id:number,bookName:string,authorName:string,quantity:number,faculty:string}>=[];
+  borrowList:Array<[]>=[];
+  stds: Array<StudentInterface> = [];
+  loginStudent: string = "name1";
+  BookSearchText: string = '';
+  bookPresent: boolean = false;
+  listOfParticularBook: Array<{ id: number, bookName: string, authorName: string, quantity: number, faculty: string }> = [];
   constructor(private api: ApiService) { }
 
   ngOnInit() {
+    const tokenFromLocalStorage = localStorage.getItem("studentArray");
+    if (tokenFromLocalStorage) {
+      this.stds = JSON.parse(tokenFromLocalStorage);
+    }
+    // else {
+    //   this.stds = [{ id: 1, name: "sth", phone: 9, semester: 8 }];//just initialize random data
+    // }
   }
 
-  searching(val:string) {
-    
-    this.BookSearchText=val;
+  searching(val: string) {
+
+    this.BookSearchText = val;
+
     // console.log(this.BookSearchText);
     this.api.getLibrarian().subscribe(res => {
-      this.librarianData = res;  
-       
-      var result = this.librarianData.map((a:any) => a.bookName);
+      this.librarianData = res;
+
+      var result = this.librarianData.map((a: any) => a.bookName);
+
 
       // console.log(result);    [cp,jh,s]
 
-for(var i=0;i<result.length;i++)
-{
-  if(this.BookSearchText==result[i])
-  {
-    this.bookPresent=true;
-    console.log(this.BookSearchText); 
-    this.listOfParticularBook.push(this.librarianData[i]);    
-    // break;
+      for (var i = 0; i < result.length; i++) {
+        if (this.BookSearchText == result[i]) {
+          this.bookPresent = true;
+          console.log(this.BookSearchText);
+          this.listOfParticularBook.push(this.librarianData[i]);
+          // break;
+        }
+      }
+
+    })
   }
-}
-     
-      })
+
+  selectStudent(std: string) {
+    this.loginStudent = std;
+    console.log(this.loginStudent);
+  }
+
+
+  //Borrow
+  borrowBook(indexOfBorrow:number){
+    // let x=[this.loginStudent, this.listOfParticularBook[indexOfBorrow]]
+    // [[student,book],[student,book],[student,book]]
+    // this.borrowList.push(x);
+    console.log(this.loginStudent);
+
+    console.log(this.listOfParticularBook[indexOfBorrow].bookName);
   }
 }
